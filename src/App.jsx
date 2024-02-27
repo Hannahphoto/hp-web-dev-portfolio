0ad619cdae13ff24ae35bad9a32d8a90a4c5e80a
Hannahphoto <hannahprice67@gmail.com>
2024-02-27 17:21:10 -0500

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import './styles/Nav.css'
 
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Footer/>
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+export default function Footer() {
+    return (
+        <div className="container text-center">
+            <div className="row align-items-end">
+                <img className="col" src="/images/github-mark-white.png" alt="git logo" id="icon"></img>
+            </div>
+        </div>
+    )
+}
