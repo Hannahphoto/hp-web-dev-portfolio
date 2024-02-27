84d6c753d5264f2035416abc73636c5b4a12b3ac
Hannahphoto <hannahprice67@gmail.com>
2024-02-27 14:44:16 -0500

    had navbar issues, now fixed, how do i style?

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+export default function NavBar() {
+    return (
+        <>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-7">Hannah Price</div>
+                    <div className="col-1">About Me</div>
+                    <div className="col-1">Portfolio</div>
+                    <div className="col-1">Contact</div>
+                    <div className="col-1">Resume</div>
+                </div>
+            </div>
+        </>
+    )
+}
