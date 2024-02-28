3e3cc8a8c4e5eeb529a946230ce6af7ec8b6ab9b
Hannahphoto <hannahprice67@gmail.com>
2024-02-27 23:46:40 -0500

    homepage, about me page at a good spot

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,37 @@
 export default function CardMe() {
     return (
         <>
-            
-            <div className="card" style={{width: "700px"}}>
-                <img className="card-img-top" src="/images/Self-Portrait_CropLoRes.jpg"  alt="self-portrait"></img>
-                <div className="card-body">
-                    {/* <h5 className="card-title">Web Developer</h5> */}
-                    <p className="card-text">I am a photographic artist with a technical background. For the last decade I have
+            <div className="card mb-3" style={{width:" 840px"}}>
+                <div className="row g-0">
+                    <div className="col-md-4">
+                        <img src="/images/Self-Portrait_CropLoRes.jpg" className="img-fluid rounded-start" alt="..."></img>
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            {/* <h5 className="card-title">Card title</h5> */}
+                            <p className="card-text">I am a photographic artist with a technical background. For the last decade I have
                         been a photo tech managing the photo & video facility at the Community College of Philadelphia.
                         I keep the facility up to date with the propper technology and software as well as repair or trouble
                         shoot any malfunctions. Opportunities in photography are sparce and I do not like being bored.
-                        have learned the skills of a Web Developer and look foward to the challenges this career entails..</p>
+                        have learned the skills of a Web Developer and look foward to the challenges this career entails.</p>
+                            {/* <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p> */}
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
-    )
+            )
 }
+
+
+            {/* <div classNameName="card" style={{width: "300px"}}>
+                <img classNameName="card-img-top" src="/images/Self-Portrait_CropLoRes.jpg"  alt="self-portrait"></img>
+                <div classNameName="card-body">
+                    {/* <h5 classNameName="card-title">Web Developer</h5> */}
+            {/* <p classNameName="card-text">I am a photographic artist with a technical background. For the last decade I have
+                        been a photo tech managing the photo & video facility at the Community College of Philadelphia.
+                        I keep the facility up to date with the propper technology and software as well as repair or trouble
+                        shoot any malfunctions. Opportunities in photography are sparce and I do not like being bored.
+                        have learned the skills of a Web Developer and look foward to the challenges this career entails.</p>
+                </div>
+            </div> */}
