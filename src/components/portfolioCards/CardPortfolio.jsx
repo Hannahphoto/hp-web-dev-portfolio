7af65c78a669b629e64caa3f004527a85495cbee
Hannahphoto <hannahprice67@gmail.com>
2024-02-28 23:34:30 -0500

    understand how to get grid to work now

--- src/components/portfolioCards/CardPortfolio.jsx
+++ src/components/portfolioCards/CardPortfolio.jsx
@@ -1,17 +1,31 @@
 export default function CardPortfolio() {
     return (
         <>
-            <div className="row row-cols-3 row-cols-md-3 g-4">
-                <div className="col-1">
+            <div className="row">
+                <div className="col-4">
                     <div className="card text-bg-transparent border-warning" style={{ width: "300px" }}>
                         <img src="/images/gods-beasts.png" className="card-img" alt="image of game" id="img"></img>
                         <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
                         </div>
                     </div>
                 </div>
+                <div className="col-4">
+                    <div className="card text-bg-transparent border-warning" style={{ width: "300px" }}>
+                        <img src="/images/movieTracks.png" className="card-img" alt="image of movie track site" id="img"></img>
+                        <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-4">
+                    <div className="card text-bg-transparent border-warning" style={{ width: "300px" }}>
+                        <img src="/images/movieTracks.png" className="card-img" alt="image of movie track site" id="img"></img>
+                        <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="row row-cols-3 row-cols-md-3 g-4">
-                <div className="col-3">
+            <div className="row">
+                <div className="col-4">
                     <div className="card text-bg-transparent border-warning" style={{ width: "300px" }}>
                         <img src="/images/movieTracks.png" className="card-img" alt="image of movie track site" id="img"></img>
                         <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
