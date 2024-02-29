86fd676e49f438c93b1978bdc180c6394c20c2e1
Hannahphoto <hannahprice67@gmail.com>
2024-02-28 21:50:50 -0500

    gods and beasts card

--- src/components/portfolioCards/CardPortfolio.jsx
+++ src/components/portfolioCards/CardPortfolio.jsx
@@ -1,11 +1,11 @@
 export default function CardPortfolio() {
     return (
         
-        <div className = "card text-bg-transparent">
+        <div className = "card text-bg-dark">
         <img src="/images/gods-beasts.png" className = "card-img" alt = "image of game" id="img"></img>
-            <div className = "card-img-overlay"style={{width:" 200px", height: "300px" }}>
-                <h5 className ="card-title" > Gods & Beasts Card Game </h5>
-                    <p className = "card-text"> This is a wider card with supporting text below as a natural lead in to additional content.This content is a little bit longer.</p>
+            <div className = "card-img-overlay bg-transparent"style={{width:" 0px", height: "0px" }}>
+                {/* <h5 className ="card-title" > Gods & Beasts Card Game </h5> */}
+                    {/* <p className = "card-text"> This is a wider card with supporting text below as a natural lead in to additional content.This content is a little bit longer.</p> */}
             </div>
         </div>
         
