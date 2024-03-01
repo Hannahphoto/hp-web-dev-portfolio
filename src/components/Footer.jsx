3cd53623553c267f14ba67af53dd2953b1335357
Hannahphoto <hannahprice67@gmail.com>
2024-02-29 23:29:17 -0500

    portfolio page progress

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,12 @@ export default function Footer() {
             <div className="row align-items-end">
                 <img className="col" src="/images/github-mark-white.png" alt="git logo" id="icon"></img>
             </div>
+            <div>
+            <img className="col" src="/images/LI-In-Bug.png" alt="linked-in logo" id="LinkdIn"></img>
+            </div>
+            {/* <div>
+            <img className="col" src="/images/dslr-camera.png" alt="camera icon" id="#photo"></img>
+            </div> */}
         </div>
     )
 }
--- src/components/portfolioCards/CardPortfolio.jsx
+++ src/components/portfolioCards/CardPortfolio.jsx
@@ -1,24 +1,25 @@
 export default function CardPortfolio() {
     return (
         <>
+            <div className="container">
             <div className="row">
                 <div className="col-4">
-                    <div className="card text-bg-transparent border-warning" style={{ width: "300px" }}>
+                    <div className="card text-bg-transparent border-success" style={{ width: "300px" }}>
                         <img src="/images/gods-beasts.png" className="card-img" alt="image of game" id="img"></img>
                         <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
                         </div>
                     </div>
                 </div>
                 <div className="col-4">
-                    <div className="card text-bg-transparent border-warning" style={{ width: "300px" }}>
+                    <div className="card text-bg-transparent border-success" style={{ width: "300px" }}>
                         <img src="/images/movieTracks.png" className="card-img" alt="image of movie track site" id="img"></img>
                         <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
                         </div>
                     </div>
                 </div>
                 <div className="col-4">
-                    <div className="card text-bg-transparent border-warning" style={{ width: "300px" }}>
-                        <img src="/images/movieTracks.png" className="card-img" alt="image of movie track site" id="img"></img>
+                    <div className="card text-bg-transparent border-success" style={{ width: "300px" }}>
+                        <img src="/images/PWA-textEditor.png" className="card-img" alt="image of PWA app" id="img"></img>
                         <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
                         </div>
                     </div>
@@ -26,12 +27,27 @@ export default function CardPortfolio() {
             </div>
             <div className="row">
                 <div className="col-4">
-                    <div className="card text-bg-transparent border-warning" style={{ width: "300px" }}>
-                        <img src="/images/movieTracks.png" className="card-img" alt="image of movie track site" id="img"></img>
+                    <div className="card text-bg-transparent border-success" style={{ width: "300px" }}>
+                        <img src="/images/weatherDashboard.png" className="card-img" alt="image of Weather Dashboard site" id="img"></img>
                         <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
                         </div>
                     </div>
                 </div>
+                <div className="col-4">
+                    <div className="card text-bg-transparent border-success" style={{ width: "300px" }}>
+                        <img src="/images/backend-noteTaker.png" className="card-img" alt="image of note taker site" id="img"></img>
+                        <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-4">
+                    <div className="card text-bg-transparent border-success" style={{ width: "300px" }}>
+                        <img src="/images/backend" className="card-img" alt="image of note taker site" id="img"></img>
+                        <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
+                        </div>
+                    </div>
+                </div>
+            </div>
             </div>
         </>
     )
