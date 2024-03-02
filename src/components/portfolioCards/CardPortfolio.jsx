2737ea28dd58d262399c587d3ded0b0be229c178
Hannahphoto <hannahprice67@gmail.com>
2024-03-01 20:08:40 -0500

    changed card border

--- src/components/portfolioCards/CardPortfolio.jsx
+++ src/components/portfolioCards/CardPortfolio.jsx
@@ -3,21 +3,21 @@ export default function CardPortfolio() {
         <>
             <div className="row">
                 <div className="col-3">
-                    <div className="card text-bg-transparent border-primary" style={{ width: "250px" }}>
+                    <div className="card text-bg-transparent border-warning" style={{ width: "250px" }}>
                         <img src="/images/gods-beasts.png" className="card-img" alt="image of game" id="img"></img>
                         <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
                         </div>
                     </div>
                 </div>
                 <div className="col-3">
-                    <div className="card text-bg-transparent border-primary" style={{ width: "250px" }}>
+                    <div className="card text-bg-transparent border-warning" style={{ width: "250px" }}>
                         <img src="/images/movieTracks.png" className="card-img" alt="image of movie track site" id="img"></img>
                         <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
                         </div>
                     </div>
                 </div>
                 <div className="col-3">
-                    <div className="card text-bg-transparent border-primary" style={{ width: "250px" }}>
+                    <div className="card text-bg-transparent border-warning" style={{ width: "250px" }}>
                         <img src="/images/PWA-textEditor.png" className="card-img" alt="image of PWA app" id="img"></img>
                         <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
                         </div>
@@ -26,21 +26,21 @@ export default function CardPortfolio() {
             </div>
             <div className="row">
                 <div className="col-3">
-                    <div className="card text-bg-transparent border-primary" style={{ width: "250px" }}>
+                    <div className="card text-bg-transparent border-warning" style={{ width: "250px" }}>
                         <img src="/images/weatherDashboard.png" className="card-img" alt="image of Weather Dashboard site" id="img"></img>
                         <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
                         </div>
                     </div>
                 </div>
                 <div className="col-3">
-                    <div className="card text-bg-transparent border-primary" style={{ width: "250px" }}>
+                    <div className="card text-bg-transparent border-warning" style={{ width: "250px" }}>
                         <img src="/images/backend-noteTaker.png" className="card-img" alt="image of note taker site" id="img"></img>
                         <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
                         </div>
                     </div>
                 </div>
                 <div className="col-3">
-                    <div className="card text-bg-transparent border-primary" style={{ width: "250px" }}>
+                    <div className="card text-bg-transparent border-warning" style={{ width: "250px" }}>
                         <img src="/images/backend" className="card-img" alt="image of note taker site" id="img"></img>
                         <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
                         </div>
