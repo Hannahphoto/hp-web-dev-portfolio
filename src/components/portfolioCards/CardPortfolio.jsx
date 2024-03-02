8e0479b46764c22dbdd4f968a1dcce9a65b25d1b
Hannahphoto <hannahprice67@gmail.com>
2024-03-01 20:06:55 -0500

    portfolio page finally formated

--- src/components/portfolioCards/CardPortfolio.jsx
+++ src/components/portfolioCards/CardPortfolio.jsx
@@ -1,24 +1,23 @@
 export default function CardPortfolio() {
     return (
         <>
-            {/* <div className="container "> */}
             <div className="row">
-                <div className="col-4">
-                    <div className="card text-bg-transparent border-primary" style={{ width: "150px" }}>
+                <div className="col-3">
+                    <div className="card text-bg-transparent border-primary" style={{ width: "250px" }}>
                         <img src="/images/gods-beasts.png" className="card-img" alt="image of game" id="img"></img>
                         <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
                         </div>
                     </div>
                 </div>
-                <div className="col-4">
-                    <div className="card text-bg-transparent border-primary" style={{ width: "200px" }}>
+                <div className="col-3">
+                    <div className="card text-bg-transparent border-primary" style={{ width: "250px" }}>
                         <img src="/images/movieTracks.png" className="card-img" alt="image of movie track site" id="img"></img>
                         <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
                         </div>
                     </div>
                 </div>
-                <div className="col-4">
-                    <div className="card text-bg-transparent border-primary" style={{ width: "200px" }}>
+                <div className="col-3">
+                    <div className="card text-bg-transparent border-primary" style={{ width: "250px" }}>
                         <img src="/images/PWA-textEditor.png" className="card-img" alt="image of PWA app" id="img"></img>
                         <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
                         </div>
@@ -26,29 +25,28 @@ export default function CardPortfolio() {
                 </div>
             </div>
             <div className="row">
-                <div className="col-4">
-                    <div className="card text-bg-transparent border-primary" style={{ width: "200px" }}>
+                <div className="col-3">
+                    <div className="card text-bg-transparent border-primary" style={{ width: "250px" }}>
                         <img src="/images/weatherDashboard.png" className="card-img" alt="image of Weather Dashboard site" id="img"></img>
                         <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
                         </div>
                     </div>
                 </div>
-                <div className="col-4">
-                    <div className="card text-bg-transparent border-primary" style={{ width: "200px" }}>
+                <div className="col-3">
+                    <div className="card text-bg-transparent border-primary" style={{ width: "250px" }}>
                         <img src="/images/backend-noteTaker.png" className="card-img" alt="image of note taker site" id="img"></img>
                         <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
                         </div>
                     </div>
                 </div>
-                <div className="col-4">
-                    <div className="card text-bg-transparent border-primary" style={{ width: "200px" }}>
+                <div className="col-3">
+                    <div className="card text-bg-transparent border-primary" style={{ width: "250px" }}>
                         <img src="/images/backend" className="card-img" alt="image of note taker site" id="img"></img>
                         <div className="card-img-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
                         </div>
                     </div>
                 </div>
             </div>
-            {/* </div> */}
         </>
     )
 }
