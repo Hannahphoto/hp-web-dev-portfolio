0f115efc53b7f14745971c27f36084861fff4e01
Hannahphoto <hannahprice67@gmail.com>
2024-03-05 12:00:02 -0500

    make mobile

--- src/components/portfolioCards/CardPortfolio.jsx
+++ src/components/portfolioCards/CardPortfolio.jsx
@@ -11,39 +11,33 @@ export default function CardPortfolio() {
         <>
             <div className="row">
                 <div className="col-3">
-                    <div className="card text-bg-transparent border-light" style={{ width: "200px" }}>
-                        <h5 className="card-title" style={{ color: "white" }}>Card Game: backend - logic</h5>
+                    <div className="card text-bg-transparent border-light" style={{ width: "230px" }}>
+                        <h5 className="card-title" style={{ color: "white" }}>Card Game: Backend - Logic</h5>
                         <Link onClick={() => openInNewTab("https://gods-and-beasts-game-15c8c3fec868.herokuapp.com/")}>
                             <img src="/images/gods-beasts.png" className="card-cardSize" alt="image of game" id="cardSize"></img>
                         </Link>
-                        <div className="card-cardSize-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
-                        </div>
                         <div className="card-body">
                         <Link onClick={()=> openInNewTab("https://github.com/ashleyh5440/gods-beasts-game")} className="card-link">Git Hub</Link>
                     </div>
                     </div>
                 </div>
                 <div className="col-3">
-                    <div className="card text-bg-transparent border-light" style={{ width: "200px" }}>
-                        <h5 className="card-title" style={{ color: "white" }}>Movie Tracks: backend - api & redesign</h5>
+                    <div className="card text-bg-transparent border-light" style={{ width: "230px" }}>
+                        <h5 className="card-title" style={{ color: "white" }}>Movie Tracks: Backend - API & Redesign</h5>
                         <Link onClick={() => openInNewTab("https://hannahphoto.github.io/movie-track/")}>
                             <img src="/images/movieTracks.png" className="card-cardSize" alt="image of movie track site" id="cardSize"></img>
                         </Link>
-                        <div className="card-cardSize-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
-                        </div>
                         <div className="card-body">
                         <Link onClick={()=> openInNewTab("https://github.com/Hannahphoto/movie-track")} className="card-link">Git Hub</Link>
                     </div>
                     </div>
                 </div>
                 <div className="col-3">
-                    <div className="card text-bg-transparent border-light" style={{ width: "200px" }}>
+                    <div className="card text-bg-transparent border-light" style={{ width: "230px" }}>
                         <h5 className="card-title" style={{ color: "white" }}>PWA</h5>
                         <Link onClick={() => openInNewTab("https://text-editor-web-application-6i87.onrender.com/")}>
                             <img src="/images/PWA-textEditor.png" className="card-cardSize" alt="image of PWA app" id="cardSize"></img>
                         </Link>
-                        <div className="card-cardSize-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
-                        </div>
                         <div className="card-body">
                         <Link onClick={()=> openInNewTab("https://github.com/Hannahphoto/pwa---textEditor")} className="card-link">Git Hub</Link>
                     </div>
@@ -52,39 +46,33 @@ export default function CardPortfolio() {
             </div>
             <div className="row">
                 <div className="col-3">
-                    <div className="card text-bg-transparent border-light" style={{ width: "200px" }}>
+                    <div className="card text-bg-transparent border-light" style={{ width: "230px" }}>
                         <h5 className="card-title" style={{ color: "white" }}>Weather Dashboard: Server Side api</h5>
                         <Link onClick={() => openInNewTab("https://hannahphoto.github.io/WhatsTheWeatherLike/")}>
                             <img src="/images/weatherDashboard.png" className="card-cardSize" alt="image of Weather Dashboard site" id="cardSize"></img>
                         </Link>
-                        <div className="card-cardSize-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
-                        </div>
                         <div className="card-body">
                         <Link onClick={()=> openInNewTab("https://github.com/Hannahphoto/WhatsTheWeatherLike")} className="card-link">Git Hub</Link>
                     </div>
                     </div>
                 </div>
                 <div className="col-3">
-                    <div className="card text-bg-transparent border-light" style={{ width: "200px" }}>
+                    <div className="card text-bg-transparent border-light" style={{ width: "230px" }}>
                         <h5 className="card-title" style={{ color: "white" }}>Note Taker: Express JS</h5>
                         <Link onClick={() => openInNewTab("https://getnotes-app-181ae37a2c03.herokuapp.com/notes")}>
                             <img src="/images/backend-noteTaker.png" className="card-cardSize" alt="image of note taker site" id="cardSize"></img>
                         </Link>
-                        <div className="card-cardSize-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
-                        </div>
                         <div className="card-body">
                         <Link onClick={()=> openInNewTab("https://github.com/Hannahphoto/backend-notetaker")} className="card-link">Git Hub</Link>
                     </div>
                     </div>
                 </div>
                 <div className="col-3">
-                    <div className="card text-bg-transparent border-light" style={{ width: "200px" }}>
-                        <h5 className="card-title" style={{ color: "white" }}>Work Schedular: Third Party api</h5>
+                    <div className="card text-bg-transparent border-light" style={{ width: "230px" }}>
+                        <h5 className="card-title" style={{ color: "white" }}>Work Schedular: Third Party API</h5>
                         <Link onClick={() => openInNewTab("https://hannahphoto.github.io/CalendarGetWorkDone-HP/")}>
                             <img src="/images/work-day.png" className="card-cardSize" alt="image of work day schedular" id="cardSize"></img>
                         </Link>
-                        <div className="card-cardSize-overlay bg-transparent" style={{ width: " 0px", height: "0px" }}>
-                        </div>
                         <div className="card-body">
                         <Link onClick={()=> openInNewTab("https://github.com/Hannahphoto/CalendarGetWorkDone-HP")} className="card-link">Git Hub</Link>
                     </div>
