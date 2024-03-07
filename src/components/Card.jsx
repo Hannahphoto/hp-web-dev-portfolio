13622cba3fa46d0512b8d8565cc77198fcc07a3c
Hannahphoto <hannahprice67@gmail.com>
2024-03-07 13:23:02 -0500

    fixed about me for phone format

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
 export default function Card() {
     return (
         <>
-            <div className="card" style={{width: "350px", height: "855px"}} id="aboutMe">
-                <img src="/images/Self-Portrait_CropLoRes.jpg" className="card-img-top" alt="self-portrait"></img>
-                    <div className="card-body">
-                        <div className="card-text">I am a photographic artist with a technical background. For the last decade I have been a photo tech managing the photo & video facility at the Community College of Philadelphia. I keep the facility up to date with the propper technology and software as well as repair or troubleshoot any malfunctions. Opportunities in photography are sparce and I do not like being bored, have learned the skills of a Web Developer and look foward to the challenges this career entails.</div>
+            <div className=""  id="">
+                <img src="/images/Self-Portrait_CropLoRes.jpg" className="aboutme-image" alt="self-portrait" id=""></img>
+                    <div className="">
+                        <div className="aboutme-text">I am a photographic artist with a technical background. For the last decade I have been a photo tech managing the photo & video facility at the Community College of Philadelphia. I keep the facility up to date with the propper technology and software as well as repair or troubleshoot any malfunctions. Opportunities in photography are sparce and I do not like being bored, have learned the skills of a Web Developer and look foward to the challenges this career entails.</div>
                     </div>
             </div>
         </>
