f34ec81a0a6eb99b4a43c7132c74ce6032a93316
Hannahphoto <hannahprice67@gmail.com>
2024-03-08 12:23:01 -0500

    typo in about me

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ export default function Card() {
             <div className=""  id="">
                 <img src="/images/Self-Portrait_CropLoRes.jpg" className="aboutme-image" alt="self-portrait" id=""></img>
                     <div className="">
-                        <div className="aboutme-text">I am a photographic artist with a technical background. For the last decade I have been a photo tech managing the photo & video facility at the Community College of Philadelphia. I keep the facility up to date with the propper technology and software as well as repair or troubleshoot any malfunctions. Opportunities in photography are sparce and I do not like being bored, have learned the skills of a Web Developer and look foward to the challenges this career entails.</div>
+                        <div className="aboutme-text">I am a photographic artist with a technical background. For the last decade I have been a photo tech managing the photo & video facility at the Community College of Philadelphia. I keep the facility up to date with the propper technology and software as well as repair or troubleshoot any malfunctions. Opportunities in photography are sparce and I do not like being bored, having learned the skills of a Web Developer, I look foward to the challenges this career entails.</div>
                     </div>
             </div>
         </>
