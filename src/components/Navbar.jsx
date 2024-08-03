b9998e66e9993e46d7b3627b4ac220c77b4d3690
Hannahphoto <hannahprice67@gmail.com>
2024-08-03 16:36:32 -0400

    fixed toggle list for iphone size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,17 +6,18 @@ export default function NavBar({ }) {
 
             <nav className="navbar navbar-expand-lg bg-body-teriary">
                 <div className="container-fluid">
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                <div className="navbar-brand col-7" href="#" style={{color: 'whitesmoke',fontSize: '3vw'}}>Hannah Price</div>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <div className="navbar-brand col-7" href="#" style={{color: 'whitesmoke',fontSize: '3vw'}}>Hannah Price</div>
-
-                        <div className="nav-item col-1"><Link id="link" to="/about">About Me</Link></div>
-                        <div className="nav-item col-1"><Link id="link" to="/portfolio">Portfolio</Link></div>
-                        <div className="nav-item col-1"><Link id="link" to="/contact">Contact</Link></div>
-                        <div className="nav-item col-1"><Link id="link" to="/resume">Resume</Link></div>
-                       
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                        
+                    <ul className="navbar-nav me-auto">
+                        <li className="nav-item col-1"><Link className="nav-link" id="link" to="/about">About Me</Link></li>
+                        <li className="nav-item col-1"><Link className="nav-link" id="link" to="/portfolio">Portfolio</Link></li>
+                        <li className="nav-item col-1"><Link className="nav-link" id="link" to="/contact">Contact</Link></li>
+                        <li className="nav-item col-1"><Link className="nav-link" id="link" to="/resume">Resume</Link></li>
+                    </ul>
                     </div>
                 </div>
             </nav>
