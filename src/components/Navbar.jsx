92b5d3cacd960a8922200081b1ce34edf2998c4c
Hannahphoto <hannahprice67@gmail.com>
2024-08-03 15:49:16 -0400

    navbar formats to phone

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,19 +3,24 @@ import { Link } from 'react-router-dom';
 export default function NavBar({ }) {
     return (
         <>
-       
-            <nav className="row navbar">
-            
-             <div className="col-7">Hannah Price</div>
-                    
-         
-                    <div className="col-1 "><Link id="link" to="/about">About Me</Link></div>
-                    <div className="col-1 "><Link id="link" to="/portfolio">Portfolio</Link></div>
-                    <div className="col-1 "><Link id="link" to="/contact">Contact</Link></div>
-                    <div className="col-1 "><Link id="link" to="/resume">Resume</Link></div>
-       
+
+            <nav className="navbar navbar-expand-lg bg-body-teriary">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                        <div className="navbar-brand col-7" href="#" style={{color: 'whitesmoke',fontSize: '3vw'}}>Hannah Price</div>
+
+                        <div className="nav-item col-1"><Link id="link" to="/about">About Me</Link></div>
+                        <div className="nav-item col-1"><Link id="link" to="/portfolio">Portfolio</Link></div>
+                        <div className="nav-item col-1"><Link id="link" to="/contact">Contact</Link></div>
+                        <div className="nav-item col-1"><Link id="link" to="/resume">Resume</Link></div>
+                       
+                    </div>
+                </div>
             </nav>
-           
+
         </>
     )
 }
