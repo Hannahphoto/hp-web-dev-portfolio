6a63bd5785d223b8e1da946cba353059e8f47551
Hannahphoto <hannahprice67@gmail.com>
2024-08-06 18:12:26 -0400

    contact form moves with width, navbar moves with width

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function NavBar({ }) {
                 <div className="container-fluid">
                 <div className="navbar-brand col-7" href="#" style={{color: 'whitesmoke',fontSize: '3vw'}}>Hannah Price</div>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation" style={{color: 'whitesmoke'}} >
-                    <span className="navbar-toggler-icon" style={{color: 'whitesmoke'}}></span>
+                    <span className="navbar-toggler-icon" style={{color: 'whitesmoke', backgroundColor: 'whitesmoke'}}></span>
                     </button>
                     <div className="collapse navbar-collapse"  id="navbarTogglerDemo02">
                         
